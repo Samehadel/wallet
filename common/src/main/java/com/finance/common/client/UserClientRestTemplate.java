060b28f0371a762b8b84fab500d592a1f8c9bdf6
Sameh.Adel <eng.sameh@outlook.com>
2025-01-09 00:05:33 +0200

    Add implementation for circuit breaker

--- common/src/main/java/com/finance/common/client/UserClientRestTemplate.java
+++ common/src/main/java/com/finance/common/client/UserClientRestTemplate.java
@@ -8,7 +8,6 @@ import com.finance.common.util.ClientUtils;
 
 import java.util.List;
 
-import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -18,58 +17,59 @@ import org.springframework.web.client.RestTemplate;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 @Service
-@RequiredArgsConstructor
 @Log4j2
+@RequiredArgsConstructor
 public class UserClientRestTemplate implements UserClient {
     private final RestTemplate restTemplate;
     private final ExceptionService exceptionService;
-    private final CircuitBreakerFactory<?, ?> circuitBreakerFactory;
 
     private static final String SERVICE_NAME = "user-svc";
     private static final String SERVICE_BASE_URL = "/api/v1/user";
     private static final String USER_CONTROLLER_BASE_URL = "/internal";
     private static final String ENDPOINT_CONTROLLER_BASE_URL = "/internal/endpoint";
-    private static final String USER_INFO_CIRCUIT_BREAKER_NAME = "userInfoCircuitBreaker";
-    private static final String ENDPOINT_CIRCUIT_BREAKER_NAME = "endpointCircuitBreaker";
 
     @Override
+    @CircuitBreaker(name = "getUserInfo", fallbackMethod = "fetchUserFallback")
     public UserDTO getUserByUsername(final String username) {
         String url = ClientUtils.constructUrl(SERVICE_NAME, SERVICE_BASE_URL, USER_CONTROLLER_BASE_URL, "/username/{username}");
 
-        return circuitBreakerFactory.create(USER_INFO_CIRCUIT_BREAKER_NAME)
-            .run(() -> callGetForUserObject(url, username),
-                throwable -> handleUserFallback(throwable, username));
+        return callGetForUserObject(url, username);
     }
 
     @Override
+    @CircuitBreaker(name = "getUserInfo", fallbackMethod = "fetchUserFallback")
     public UserDTO getUserByMobile(final String mobile) {
         String url = ClientUtils.constructUrl(SERVICE_NAME, SERVICE_BASE_URL, USER_CONTROLLER_BASE_URL, "/mobile/{mobile}");
 
-        return circuitBreakerFactory.create(USER_INFO_CIRCUIT_BREAKER_NAME)
-            .run(() -> callGetForUserObject(url, mobile),
-                throwable -> handleUserFallback(throwable, mobile));
+        return callGetForUserObject(url, mobile);
     }
 
     private UserDTO callGetForUserObject(final String url, final Object... uriVariables) {
         return restTemplate.getForObject(url, UserDTO.class, uriVariables);
     }
 
-    @Override
-    public List<AccessUrlDTO> getPublicEndpoints() {
-        String url = ClientUtils.constructUrl(SERVICE_NAME, SERVICE_BASE_URL, ENDPOINT_CONTROLLER_BASE_URL, null);
-
-        return circuitBreakerFactory.create(ENDPOINT_CIRCUIT_BREAKER_NAME)
-            .run(() -> callGetForParameterizedType(url, getAccessUrlResponseType()), this::handleEndpointsFallback);
+    private UserDTO fetchUserFallback(final Exception e) {
+        log.error("Error occurred while fetching user due to: [{}]", e.getMessage());
+        throw exceptionService.buildInternalException(SharedApplicationError.GENERIC_ERROR);
     }
 
     @Override
+    @CircuitBreaker(name = "getEndpoints", fallbackMethod = "fetchEndpointsFallback")
+    public List<AccessUrlDTO> getPublicEndpoints() {
+        String url = ClientUtils.constructUrl(SERVICE_NAME, SERVICE_BASE_URL, ENDPOINT_CONTROLLER_BASE_URL, null);
+
+        return callGetForParameterizedType(url, getAccessUrlResponseType());
+    }
+
+    @Override
+    @CircuitBreaker(name = "getEndpoints", fallbackMethod = "fetchEndpointsFallback")
     public List<AccessUrlDTO> getUserEndpoints(final String username) {
         String url = ClientUtils.constructUrl(SERVICE_NAME, SERVICE_BASE_URL, ENDPOINT_CONTROLLER_BASE_URL, "/username/{username}");
 
-        return circuitBreakerFactory.create(ENDPOINT_CIRCUIT_BREAKER_NAME)
-            .run(() -> callGetForParameterizedType(url, getAccessUrlResponseType(), username),
-                this::handleEndpointsFallback);
+        return callGetForParameterizedType(url, getAccessUrlResponseType(), username);
     }
 
     private <R> R callGetForParameterizedType(final String url, final ParameterizedTypeReference<R> responseType, final Object... uriVariables) {
@@ -87,15 +87,8 @@ public class UserClientRestTemplate implements UserClient {
         };
     }
 
-    private UserDTO handleUserFallback(final Throwable throwable, final String identifier) {
-        log.error("Circuit breaker fallback for user lookup with identifier: {}", identifier, throwable);
-
-        throw exceptionService.buildServiceUnavailableException(SharedApplicationError.GENERIC_ERROR);
-    }
-
-    private List<AccessUrlDTO> handleEndpointsFallback(final Throwable throwable) {
-        log.error("Circuit breaker fallback for user endpoints lookup [{}]", throwable.getMessage());
-
-        throw exceptionService.buildServiceUnavailableException(SharedApplicationError.GENERIC_ERROR);
+    private List<AccessUrlDTO> fetchEndpointsFallback(final Exception e) {
+        log.error("Error occurred while fetching endpoints due to: [{}]", e.getMessage());
+        throw exceptionService.buildInternalException(SharedApplicationError.GENERIC_ERROR);
     }
 }
