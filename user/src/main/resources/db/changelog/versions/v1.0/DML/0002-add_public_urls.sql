ab16a4962eed621217e13eeb5eca0875004d2dfd
samehadel <eng.sameh9@outlook.com>
2025-11-07 11:46:42 +0200

    replace sqp server with postgres

--- user/src/main/resources/db/changelog/versions/v1.0/DML/0002-add_public_urls.sql
+++ user/src/main/resources/db/changelog/versions/v1.0/DML/0002-add_public_urls.sql
@@ -1,8 +1,8 @@
 -- liquibase formatted sql
 -- changeset Sameh.Adel:0002-add_public_urls
 
-INSERT INTO ACCESS_URL(URL, METHOD, PRIVATE)
-VALUES ('/user/public/register', 'POST', 0);
+INSERT INTO ACCESS_URL (URL, METHOD, PRIVATE)
+VALUES ('/user/public/register', 'POST', FALSE);
 
-INSERT INTO ACCESS_URL(URL, METHOD, PRIVATE)
-VALUES ('/security/public/authenticate', 'POST', 0);
+INSERT INTO ACCESS_URL (URL, METHOD, PRIVATE)
+VALUES ('/security/public/authenticate', 'POST', FALSE);
