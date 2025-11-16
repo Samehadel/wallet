f5bd00d8eab6848e5449acf521f468afc313b6f4
samehadel <eng.sameh9@outlook.com>
2025-11-16 16:07:58 +0200

    Adapted to the new EventProducer design

--- security/src/main/java/com/finance/security/event/UserEventProducer.java
+++ security/src/main/java/com/finance/security/event/UserEventProducer.java
@@ -3,16 +3,15 @@ package com.finance.security.event;
 
 import com.finance.common.constants.TopicsNames;
 import com.finance.common.event.EventProducer;
+import com.finance.common.event.schema.user.Event;
 import com.finance.common.event.schema.user.EventTypes;
 import com.finance.common.event.schema.user.UserStatus;
 import com.finance.common.event.schema.user.UserStatusMessage;
 import com.finance.common.util.StringUtil;
-
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.stereotype.Service;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
@@ -35,10 +34,9 @@ public class UserEventProducer {
 
     private void pushEvent(final Long userId, final UserStatus userStatus, final EventTypes eventType, final String reason) {
         UserStatusMessage userStatusMessage = UserStatusMessage.newBuilder()
-            .setEventType(eventType)
-            .setUserId(userId)
-            .setNewStatusTime(System.currentTimeMillis())
-            .build();
+                .setUserId(userId)
+                .setNewStatusTime(System.currentTimeMillis())
+                .build();
 
         if (userStatus != null) {
             userStatusMessage = userStatusMessage.toBuilder().setNewStatus(userStatus).build();
@@ -48,6 +46,11 @@ public class UserEventProducer {
             userStatusMessage = userStatusMessage.toBuilder().setReason(reason).build();
         }
 
-        eventProducer.postEvent(TopicsNames.USER_STATUS_UPDATE_TOPIC, userStatusMessage);
+        Event event = Event.newBuilder()
+                .setUserStatusMessage(userStatusMessage)
+                .setEventType(eventType)
+                .setTimestamp(System.currentTimeMillis())
+                .build();
+        eventProducer.postEvent(TopicsNames.USER_STATUS_UPDATE_TOPIC, event);
     }
 }
