63ec352bb6b4cce6690a8c47d9da5ad3042febb6
samehadel <eng.sameh9@outlook.com>
2026-01-07 14:49:47 +0200

    Update RedisEventProducer to use MapRecord

--- common/src/main/java/com/finance/common/event/RedisEventProducer.java
+++ common/src/main/java/com/finance/common/event/RedisEventProducer.java
@@ -4,10 +4,15 @@ import com.finance.common.event.schema.user.Event;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.data.redis.connection.stream.MapRecord;
 import org.springframework.data.redis.connection.stream.ObjectRecord;
 import org.springframework.data.redis.connection.stream.StreamRecords;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 @ConditionalOnProperty(value = "event.message.broker", havingValue = "redis")
@@ -18,10 +23,10 @@ public class RedisEventProducer implements EventProducer {
 
     @Override
     public void postEvent(String topic, Event event) {
-        ObjectRecord<String, Event> record = StreamRecords.newRecord()
-                .ofObject(event)
-                .withStreamKey(topic);
+        Map<byte[], byte[]> raw = new HashMap<>();
+        raw.put("value".getBytes(StandardCharsets.UTF_8), event.toByteArray());
 
-        redisTemplate.opsForStream().add(record);
+        MapRecord<String, byte[], byte[]> byteRecord = StreamRecords.rawBytes(raw).withStreamKey(topic);
+        redisTemplate.opsForStream().add(byteRecord);
     }
 }
