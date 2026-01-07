1fde0601cb990c35035a7d5b6b1d670aa001cacf
samehadel <eng.sameh9@outlook.com>
2026-01-07 14:48:50 +0200

    Update request mapping for BaseController

--- common/src/main/java/com/finance/common/controller/BaseController.java
+++ common/src/main/java/com/finance/common/controller/BaseController.java
@@ -10,12 +10,12 @@ import org.springframework.web.bind.annotation.*;
 public abstract class BaseController<D extends BaseDTO> {
 	protected abstract BaseService<D> getService();
 
-	@PostMapping("/create")
+	@PostMapping
 	public ApiResponse<D> create(@RequestBody D request) {
 		return getService().create(request);
 	}
 
-	@GetMapping("/get/{code}")
+	@GetMapping("/{code}")
 	public ApiResponse<D> get(@PathVariable("code") String code) {
 		return getService().get(code);
 	}
