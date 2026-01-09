d835f05f2ae094f4a10ba89f9bd99f36bf894066
samehadel <eng.sameh9@outlook.com>
2026-01-09 03:30:18 +0200

    Fix code - Round 2

--- account/src/main/java/com/finance/account/mapper/AccountMapper.java
+++ account/src/main/java/com/finance/account/mapper/AccountMapper.java
@@ -4,32 +4,44 @@ import com.finance.account.entity.AccountEntity;
 import com.finance.common.dto.AccountDTO;
 import com.finance.common.mapper.GlobalMapper;
 
+import java.util.List;
+
 public class AccountMapper implements GlobalMapper <AccountEntity, AccountDTO> {
 
 	@Override
 	public AccountEntity mapToEntity(AccountDTO dto) {
-		AccountEntity entity = AccountEntity.builder()
-				.customerCode(dto.getCustomerCode())
-				.accountType(dto.getAccountType())
-				.accountHolderName(dto.getAccountHolderName())
-				.build();
+        return AccountEntity.builder()
+                .customerCode(dto.getCustomerCode())
+                .accountType(dto.getAccountType())
+                .accountHolderName(dto.getAccountHolderName())
+                .build();
+	}
 
-		return entity;
+	@Override
+	public List<AccountEntity> mapToEntity(List<AccountDTO> dto) {
+		return dto.stream()
+				.map(this::mapToEntity)
+				.toList();
 	}
 
 	@Override
 	public AccountDTO mapToDTO(AccountEntity entity) {
-		AccountDTO dto = AccountDTO.builder()
-				.customerCode(entity.getCustomerCode())
-				.accountNumber(entity.getAccountNumber())
-				.accountType(entity.getAccountType())
-				.accountStatus(entity.getAccountStatus())
-				.accountHolderName(entity.getAccountHolderName())
-				.monthlyLimit(entity.getMonthlyLimit())
-				.dailyLimit(entity.getDailyLimit())
-				.balance(entity.getBalance())
-				.build();
+        return AccountDTO.builder()
+                .customerCode(entity.getCustomerCode())
+                .accountNumber(entity.getAccountNumber())
+                .accountType(entity.getAccountType())
+                .accountStatus(entity.getAccountStatus())
+                .accountHolderName(entity.getAccountHolderName())
+                .monthlyLimit(entity.getMonthlyLimit())
+                .dailyLimit(entity.getDailyLimit())
+                .balance(entity.getBalance())
+                .build();
+	}
 
-		return dto;
+	@Override
+	public List<AccountDTO> mapToDTO(List<AccountEntity> entity) {
+		return entity.stream()
+				.map(this::mapToDTO)
+				.toList();
 	}
 }
