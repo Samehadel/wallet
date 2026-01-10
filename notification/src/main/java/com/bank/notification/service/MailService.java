59679c38bc92fd47d7522399859af9cdc052f1ae
samehadel <eng.sameh9@outlook.com>
2026-01-11 00:36:56 +0200

    Fix code - Remove Account Service

--- notification/src/main/java/com/bank/notification/service/MailService.java
+++
@@ -1,5 +0,0 @@
-package com.bank.notification.service;
-
-public interface MailService {
-	void sendMail(String message, String email);
-}
--- notification/src/main/java/com/bank/notification/service/SMSService.java
+++
@@ -1,5 +0,0 @@
-package com.bank.notification.service;
-
-public interface SMSService {
-	void sendSMS(String message, String phoneNumber);
-}
